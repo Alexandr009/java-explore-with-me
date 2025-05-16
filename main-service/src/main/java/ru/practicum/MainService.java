5804f9d69b4c3287d002f39e65008069354c9352
Aleksandr.Abramovich <hause.abramovi44@gmail.com>
2025-05-16 20:26:08 +0300

    - add docker for main service

---
+++ main-service/src/main/java/ru/practicum/MainService.java
@@ -0,0 +1,8 @@
+package ru.practicum;
+
+@SpringBootApplication
+public class MainService {
+    public static void main(String[] args) {
+        SpringApplication.run(MainService.class, args);
+    }
+}
