5f9b07a0c6ada611c709e000327712a3af1c4a97
Aleksandr.Abramovich <hause.abramovi44@gmail.com>
2025-06-03 11:18:22 +0300

    - fixed checkstyle

--- main-service/src/main/java/ru/practicum/category/dto/CategoryPostDto.java
+++ main-service/src/main/java/ru/practicum/category/dto/CategoryPostDto.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.antlr.v4.runtime.misc.NotNull;
 
 @Data
 @AllArgsConstructor
--- main-service/src/main/java/ru/practicum/user/dto/UserPostDto.java
+++ main-service/src/main/java/ru/practicum/user/dto/UserPostDto.java
@@ -4,7 +4,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
